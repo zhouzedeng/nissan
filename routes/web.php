5c4cb378792148662ba752edac90d7ac1e7951d4
zhouzedeng <zhouzedeng@huishoubao.com.cn>
2018-10-27 18:28:50 +0800

    修改路由

--- routes/web.php
+++ routes/web.php
@@ -21,26 +21,9 @@ Route::get('getCouponList', 'CouponController@getCouponList')->name('getCouponLi
 
 
 Route::get('/', 'HomeController@index')->name('home.index');
-/**********************************  问题模块  ******************************************************/
-Route::group(['namespace' => 'Question'], function(){
-    Route::get('question/index', 'QuestionController@index')->name('question.index');
-    Route::get('question/list', 'QuestionController@getList')->name('question.list');
-    Route::get('question/add', 'QuestionController@add')->name('question.add');
-    Route::post('question/save', 'QuestionController@save')->name('question.save');
-    Route::get('question/export', 'QuestionController@export')->name('question.export');
-});
-
-/**********************************  用户模块  ******************************************************/
-Route::group(['namespace' => 'User'], function(){
-    Route::get('user/index', 'UserController@index')->name('user.index');
-    Route::get('user/list', 'UserController@getList')->name('user.list');
-    Route::get('user/add', 'UserController@add')->name('user.add');
-    Route::post('user/save', 'UserController@save')->name('user.save');
-    Route::get('user/export', 'UserController@export')->name('user.export');
-});
 
 
-/**********************************  上传模块  ******************************************************/
-Route::group(['namespace' => 'Upload'], function(){
-    Route::post('upload', 'UploadController@upload')->name('upload');
-});
+
+
+
+
